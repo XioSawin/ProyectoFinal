c0c4c99ad6cbd12e11d256139414f29045140650
Xiomara S <42756313+XioSawin@users.noreply.github.com>
2021-08-09 00:33:22 -0300

    Delete unnecessary comments

--- routes/mainRoutes.js
+++ routes/mainRoutes.js
@@ -1,9 +1,6 @@
 const express = require('express');
 const app = express();
 
-//const passport = require('passport');
-//const LocalStrategy = require('passport-local').Strategy;
-
 const router = express.Router();
 
 const mainController = require('../controllers/mainController');
@@ -20,36 +17,6 @@ app.set("view engine", "hbs");
 app.set("views", "./views");
 
 app.use(express.static('public'));
-//app.use(passport.initialize());
-//app.use(passport.session());
-
-
-
-
-/* ----------------------- SERIALIZE & DESERIALIZE ----------------------- */
-
-/*
-passport.serializeUser(function(user, cb) {
-        cb(null, user);
-});
-
-passport.deserializeUser(function(obj, cb) {
-        cb(null, obj);
-});
-
-*/
-
-/* ----------------------- LOGIN ----------------------- */
-
-/* -------------- local strategy -------------- */
-/*
-passport.use('login', new LocalStrategy({
-    passReqToCallback: true
-},
-    mainController.loginUser(req, username, password, done)
-)
-);
-*/
 
 /* -------------- routes -------------- */
 
@@ -60,7 +27,5 @@ router.post('/login', mainController.logUser, mainController.Redirect);
 router.get('/faillogin', mainController.LoginFail);
 
 router.get('/logout', mainController.Logout);
-
-//router.post('/order', mainController.Order);
 
 module.exports = router;
