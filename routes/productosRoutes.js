73718b4727667bb34c5c9891694d49438f1761aa
Xiomara S <42756313+XioSawin@users.noreply.github.com>
2021-08-09 00:33:51 -0300

    Delete unnecessary comments

--- routes/productosRoutes.js
+++ routes/productosRoutes.js
@@ -1,16 +1,10 @@
 // required
-//const mongoose = require('mongoose');
 const express = require('express');
-// const app = express();
-
 const router = express.Router();
 
 const productsController = require('../controllers/productsController');
 const auth = require('../middleware/auth');
 
-
-// administrador
-//bconst administrador = true;
 
 // routes
 
