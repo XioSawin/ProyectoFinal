8bcaab970b6583a79f45d9aa71542e2f3f747f2e
Xiomara S <42756313+XioSawin@users.noreply.github.com>
2021-08-17 11:36:37 -0300

    Delete unnecessary comments.

--- controllers/mensajesController.js
+++ controllers/mensajesController.js
@@ -1,12 +1,6 @@
 const express = require('express');
 const app = express();
 
-//const http = require('http');
-//const server = http.createServer(app);
-
-//const { Socket } = require('dgram');
-//const io = require('socket.io')(http);
-
 const handlebars = require('express-handlebars');
 
 app.use(express.json())
@@ -28,31 +22,6 @@ app.use(express.static('public'));
 
 const messagesModel = require('../models/mensajes');
 
-/*
-const getMessages = () => {
-    const messages = messagesModel.find({});
-
-    return messages;
-}
-
-io.on('connection', (socket) => {
-    socket.emit('messages', getMessages());
-
-    socket.on('new-message', function(data) {
-
-        const messageSaved = new messagesModel(data);
-
-        messageSaved.save()
-            .then( () => res.sendStatus(201) )
-            .catch( (err) => res.status(400).json({
-                status: 400,
-                message: 'Error guardando mensaje en base de datos'
-            }));
-        
-        io.sockets.emit('messages', getMessages());
-    })
-})
-*/
 const getChat = async(req, res) => {
     res.render("chat", {});
 }
