7fa898289c7ed15bf10aa1513312d6fdc820c7d8
Xiomara S <42756313+XioSawin@users.noreply.github.com>
2021-08-17 11:39:29 -0300

    Delete unnecessary comments.

--- routes/mensajesRoutes.js
+++ routes/mensajesRoutes.js
@@ -3,16 +3,11 @@ const router = express.Router();
 const app = express();
 
 app.use(express.json())
+
 const mensajesController = require('../controllers/mensajesController');
 
 router.get('/', mensajesController.getChat);
 
 router.get('/:email', mensajesController.getMsgByEmail);
-
-//router.post('/:email', mensajesController.postMessage);
-
-//router.patch('/:id', auth, productsController.updateProduct);
-
-//router.delete('/:id', auth, productsController.deleteProduct);
 
 module.exports = router;
